cec705178de56411934c3d02a0a98fdcbfbf6dae
Zakkios <laskari.zakaria@gmail.com>
2023-07-12 09:30:55 +0200

    init react project with npx

--- App.js
+++
@@ -1,7 +0,0 @@
-const element = React.createElement("h1", {}, "Hello React !");
-
-const container = document.getElementById("root");
-
-const root = ReactDOM.createRoot(container);
-
-root.render(element);
